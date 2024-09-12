2cb5bbd1a539562fb33501fd8f008fc87265ce06
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-09-12 15:08:20 +0300

    Update ExitWin.cpp

--- ExitWindows/ExitWin.cpp
+++ ExitWindows/ExitWin.cpp
@@ -1,63 +1,88 @@
-﻿#define ERR_T L"Error"
 #include <windows.h>
 #include <winuser.h>
+#include <cwchar>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, const PSTR szCmdLine, int iCmdShow)
+#define ERR_T L"Error"
+#define MB_OK_ERROR (MB_OK | MB_ICONERROR)
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR szCmdLine, int iCmdShow)
 {
     UINT ewxFlags = 0;
     HANDLE hToken;
     TOKEN_PRIVILEGES tkp;
 
-    if (szCmdLine == NULL || *szCmdLine == '\0')
+    if (szCmdLine == NULL || *szCmdLine == L'\0')
     {
-        MessageBox(NULL, L"No command line argument specified.", ERR_T, MB_OK);
+        MessageBox(NULL, L"No command line argument specified.", ERR_T, MB_OK_ERROR);
         return 1;
     }
 
-    char* szCmdLower = static_cast<char*>(malloc(strlen(szCmdLine) + 1));
-    strcpy_s(szCmdLower, strlen(szCmdLine), szCmdLine);
-    for (int i = 0; i < strlen(szCmdLower); i++)
-        szCmdLower[i] = tolower(szCmdLower[i]);
+    size_t len = wcslen(szCmdLine);
+    wchar_t* szCmdLower = static_cast<wchar_t*>(malloc((len + 1) * sizeof(wchar_t)));
+    if (!szCmdLower) {
+        MessageBox(NULL, L"Memory allocation failed.", ERR_T, MB_OK_ERROR);
+        return 5;
+    }
+    wcscpy_s(szCmdLower, len + 1, szCmdLine);
+    for (size_t i = 0; i < len; i++)
+        szCmdLower[i] = towlower(szCmdLower[i]);
 
-    switch (szCmdLower)
+    if (len == 1)
     {
-    case L"s":
-        ewxFlags = EWX_SHUTDOWN;
-        break;
-    case L"r":
-        ewxFlags = EWX_REBOOT;
-        break;
-    case L"a":
-        ewxFlags = EWX_RESTARTAPPS;
-        break;
-    case L"l":
-        ewxFlags = EWX_LOGOFF;
-        break;
-    case L"h":
-        ewxFlags = EWX_HYBRID_SHUTDOWN;
-        break;
-    default:
-        MessageBox(NULL, L"Invalid command line argument.", ERR_T, MB_OK);
+        switch (szCmdLower[0])
+        {
+        case L's':
+            ewxFlags = EWX_SHUTDOWN;
+            break;
+        case L'r':
+            ewxFlags = EWX_REBOOT;
+            break;
+        case L'a':
+            ewxFlags = EWX_RESTARTAPPS;
+            break;
+        case L'l':
+            ewxFlags = EWX_LOGOFF;
+            break;
+        case L'h':
+            ewxFlags = EWX_HYBRID_SHUTDOWN;
+            break;
+        default:
+            MessageBox(NULL, L"Invalid command line argument.", ERR_T, MB_OK_ERROR);
+            free(szCmdLower);
+            return 2;
+        }
+    }
+    else
+    {
+        MessageBox(NULL, L"Invalid command line argument.", ERR_T, MB_OK_ERROR);
+        free(szCmdLower);
         return 2;
     }
 
-    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
-        MessageBox(NULL, L"Failed to access process token.", ERR_T, MB_OK);
+    free(szCmdLower);
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+    {
+        MessageBox(NULL, L"Failed to access process token.", ERR_T, MB_OK_ERROR);
         return 3;
     }
 
-    LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
+    if (!LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid)) {
+        MessageBox(NULL, L"Failed to lookup privilege value.", ERR_T, MB_OK_ERROR);
+        return 3;
+    }
     tkp.PrivilegeCount = 1;
     tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
 
-    OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken);
-    LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &tkp.Privileges[0].Luid);
-    tkp.PrivilegeCount = 1;
-    tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
     AdjustTokenPrivileges(hToken, FALSE, &tkp, 0, (PTOKEN_PRIVILEGES)nullptr, 0);
-    if(ExitWindowsEx(ewxFlags, 0) == 0)
+    if (GetLastError() != ERROR_SUCCESS) {
+        MessageBox(NULL, L"Failed to adjust token privileges.", ERR_T, MB_OK_ERROR);
+        return 3;
+    }
+
+    if (ExitWindowsEx(ewxFlags, 0) == 0)
     {
-        MessageBox(NULL, L"Shutdown cannot be initiated.", ERR_T, MB_OK);
+        MessageBox(NULL, L"Shutdown cannot be initiated.", ERR_T, MB_OK_ERROR);
         return 4;
     }
 
