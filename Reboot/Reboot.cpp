008812d802870d8b47951759af8ab3513f597c27
Pavel Bashkardin <90511962+ng256@users.noreply.github.com>
2024-10-29 15:51:13 +0300

    Update Reboot.cpp

--- Reboot/Reboot.cpp
+++ Reboot/Reboot.cpp
@@ -1,4 +1,13 @@
-/*
+/***********************************************************
+This   program  demonstrates    a  method  to    initiate  a
+system  reboot  on  Windows   by   adjusting  the  process's
+privileges  to  enable     the required  shutdown privilege.
+It   first  retrieves  the  current   process's    token and
+elevates  its  privileges  by enabling "SeShutdownPrivilege"
+using native  Windows API calls.  Once   the  privilege   is
+granted,   the  program  attempts  to   initiate   a  system
+reboot.
+
 Distributed under MIT License:                             
 
 Copyright (c) 2024 Pavel Bashkardin
@@ -10,11 +19,11 @@ Software without restriction,  including without  limitation
 the rights to use, copy, modify, merge, publish, distribute,
 sublicense,  and/or sell copies   of the  Software,   and to
 permit  persons to whom  the Software is furnished to do so,
-subject      to        the       following       conditions:
+subject to the following conditions:
 
 1.  The above copyright  notice and this   permission notice
 shall be included  in  all copies or substantial portions of
-the                                                Software.
+the Software.
 
 2. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
 KIND, EXPRESS OR IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE
@@ -24,7 +33,7 @@ OR  COPYRIGHT  HOLDERS  BE LIABLE  FOR ANY CLAIM, DAMAGES OR
 OTHER LIABILITY, WHETHER IN AN  ACTION OF CONTRACT,  TORT OR
 OTHERWISE,  ARISING FROM, OUT OF  OR  IN CONNECTION WITH THE
 SOFTWARE  OR THE  USE   OR OTHER DEALINGS IN   THE SOFTWARE.
-*/
+***********************************************************/
 
 #include <windows.h>
 #include <winuser.h>
